98e4059728088d2a84cc7d7b5abb41903fe6efd0
snehasonwane <snehasonwane01@gmail.com>
2022-08-19 11:29:42 +0530

    [Manasi] bookstore view details page

--- Js/dashboard.js
+++ Js/dashboard.js
@@ -1,10 +1,8 @@
-function myFunction() {
-  window.open("displaybook.html");
-}
+
 var result;
 let token = localStorage.getItem('token');
-getNotes();
-function getNotes() {
+getbooks();
+function getbooks() {
   $.ajax({
     url: 'https://bookstore.incubation.bridgelabz.com/bookstore_user/get/book',
     type: 'GET',
@@ -16,7 +14,7 @@ function getNotes() {
       document.getElementById('demo').innerHTML = bookarray.map((book) => `
         <div class="box">
         <div class="imgContainer" >
-        <img onclick="myFunction()" id="Itrash" src="../assets/Image 11.png" style="height:160px;width:140px; margin-top:23px;">
+        <img onclick="myFunction()" id="${book._id}" src="../assets/Image 11.png" style="height:160px;width:140px; margin-top:23px;">
         </div>
         <div class="mt-3 pl-4" style="height:99px; background-color:#fff">
         <p class="mb-0"><b>${book.bookName}</b></p>
@@ -36,31 +34,37 @@ function getNotes() {
     },
     error: function (error) {
       console.log(error);
-
     }
   });
 
-  const id = localStorage.getItem('id');
-  const bookName = localStorage.getItem('id');
-  const author = localStorage.getItem('author');
-  const price = localStorage.getItem('price');
-  const discountPrice = localStorage.getItem('discountPrice'); 
+  // $(document).on('click', '.box', function (event) {
+  //   console.log(event.target.id)
+  //   console.log(event.target.textContent)
 
-  window.localStorage.setItem('bookName', 'id')
+  //   window.location = `http://localhost:5501/Pages/displayBook.html?id=${event.target.id}`
+  // })
+
+//   const id = localStorage.getItem('id');
+//   const bookName = localStorage.getItem('id');
+//   const author = localStorage.getItem('author');
+//   const price = localStorage.getItem('price');
+//   const discountPrice = localStorage.getItem('discountPrice'); 
+
+//   window.localStorage.setItem('bookName', 'id')
 
 
-  const myObject = {
-    id:id,
-    bookname : bookName,
-    author : author,
-    price : price,
-    discountPrice : discountPrice
-  }
+//   const myObject = {
+//     id:id,
+//     bookname : bookName,
+//     author : author,
+//     price : price,
+//     discountPrice : discountPrice
+//   }
  
-  window.localStorage.setItem("myObject", JSON.stringify(myObject));
+//   window.localStorage.setItem("myObject", JSON.stringify(myObject));
 
-  let newObject = window.localStorage.getItem("myObject");
-console.log(JSON.parse(newObject));
+//   let newObject = window.localStorage.getItem("myObject");
+// console.log(JSON.parse(newObject));
 
 // getNotes();
 // function getNotes() {
@@ -104,3 +108,16 @@ console.log(JSON.parse(newObject));
 //<div class="book"id=${book.bookImage}><img src="/Assets/bookListing.png" class ="img"> </div>
 {/* <span class="text-center" style="width:3px; height:22px;background-color:green;border-radius:3px;">4.5</span>
 <p class="mb-4"><b>Rs.${book.discountPrice} </b> Rs.${book.price}</p> */}
+
+
+function myFunction() {
+  
+document.getElementById('demo').addEventListener('click',(f)=>{
+console.log(f.target.id)
+localStorage.setItem('bookid', f.target.id)
+window.open("displaybook.html");
+  })
+   
+
+  
+}
--- Js/displaybook.js
+++ Js/displaybook.js
@@ -1,108 +1,87 @@
-// window.addEventListener('DOMContentLoaded', function () {
-//   const urlSearchParams = new URLSearchParams(window.location.search);
-//   console.log(urlSearchParams)
-// const flxContainer = document.querySelector('.flex-containernew')
 
-
-// var result;
-// const params = Object.fromEntries(urlSearchParams.entries());
-// console.log(params.id);
-// getNotes();
-// function getNotes() {
-//   $.ajax({
-//     url: 'https://bookstore.incubation.bridgelabz.com/bookstore_user/get/book',
-//     type: 'GET',
-//     'Content-type': 'application/json',
-//     // 'x-access-token': this.token,
-//     success: function (result) {
-//       console.log(result.params.id);
-//       bookarray = result.params.id;
-//       let array = res.filter(function (book1) {
-//       return book1.id == params.id;
-// })
-//       console.log(filterArry)
-//       console.log(params.id)
+var result;
+let token = localStorage.getItem('token');
+let id = localStorage.getItem('bookid');
+console.log(id)
+getbooks();
+function getbooks() {
+  $.ajax({
+    url: 'https://bookstore.incubation.bridgelabz.com/bookstore_user/get/book',
+    type: 'GET',
+    'Content-type': 'application/json',
+    // 'x-access-token': this.token,
+    success: function (result) {
+      console.log(result.result);
+      const bookarray = result.result;
+        let array = bookarray.filter(function (book1) {
+            return book1._id === id;
+            // return book1.id == "616fdef274dbc1000e41e65b";
+      })
+      console.log(array)
+      //  array.innerHTML = filterArry.map(function (book1) {
+       document.getElementById('demo').innerHTML = array.map((book1)=> 
+        `<div class="bookDesc1">
+        <div class="bookName1">${book1.bookName}</div>
+        <div class="author1" id="author">${book1.author}</div>
+        <div class="rating1">
+            <div class="star1" id=${book1.id}>4.5*</div>
+            <div class="starRating1" id=${book1.id}>(20)</div>
+        </div>
+        <div class="price1">
+            <div class="price11" id=${book1.id}>Rs. ${book1.discountPrice}</div>
+            <div class="originalPrice1" id=${book1.id}> ${book1.price}</div>
+        </div>
+    </div>
+    <div class="bookDetails">
+        <div class="bk1">Book Detail</div>
+        <div class= "bk2"><p>${book1.description} </p></div>
+    </div>`
+       )
+ }
+})
+}
+ 
+// let bookarray = localStorage.getItem('bookarray')
+// console.log(bookarray)
+// function match(id){
+//   return  bookid
+// }
+//   function match(id, bookarray) {
+//     return bookarray.filter(function(bookid) {
+//       if(bookid.id == id) {
+//         return bookid 
+//       }
+//     })
+//   }
+    
+// const item = bookarray.filter (myfunction);        
+// document.getElementById("demo").innerHTML =item;
+// function myfunction(bookid,bookname){
+//   return id == bookid;
+// }
+   
+        // var item = bookarray.filter(function (book) {
+        //     return book.id === id;
+        // });
       
-//      array.innerHTML = filterArry.map(function(book1) {
-//       return `<div class="bookDesc1">
-//       <div class="bookName1" id="bookName">${book1.bookName}</div>
-//       <div class="author1" id="author">${book1.author}</div>
-//       <div class="rating1">
-//       <div class="star1" id=${book1._id}>4.5*</div>
-//       <div class="starRating1" id=${book1._id}>(20)</div>
-//     </div>
-//     <div class="price1">
-//         <div class="price11" id=${book1._id}>Rs. ${book1.discountPrice}</div>
-//         <div class="originalPrice1" id=${book1._id}> ${book1.price}</div>
-//     </div>
-// </div>
-// <div class="bookDetails">
-//     <div class="bk1">Book Detail</div>
-//     <div class= "bk2"><p>${book1.description} </p></div>
-// </div>`
-// })
-//     }
-//   })
-// }
-// })
 
-// var result;
-// let token = localStorage.getItem('token');
-// getNotes();
-// function getNotes() {
-//   $.ajax({
-//     url: 'https://bookstore.incubation.bridgelabz.com/bookstore_user/get/book',
-//     type: 'GET',
-//     'Content-type': 'application/json',
-//     // 'x-access-token': this.token,
-//     success: function (result) {
-//       console.log(result.result);
-//       bookarray = result.result._id;
-//       document.getElementById('div2').innerHTML = bookarray.map((book1) => `
-//       <div class="bookDesc1">
-//     <div class="bookName1" id="bookName">${book1.bookName}</div>
-//     <div class="author1" id="author">${book1.author}</div>
-//     <div class="rating1">
-//         <div class="star1">4.5*</div>
-//         <div class="starRating1">(20)</div>
-//     </div>
-//     <div class="price1">
-//         <div class="price11">Rs. ${book1.discountPrice}</div>
-//         <div class="originalPrice1"> ${book1.price}</div>
-//     </div>
-// </div>
-// <div class="bookDetails">
-//     <div class="bk1">Book Detail</div>
-//     <div class= "bk2"><p><p>${book1.description}</p></div>
-// </div>`
-// )
-// },
-// error: function (error) {
-//   console.log(error);
-
-// }
-// });
-// }
-// var result;
-// document.getElementById('demo').innerHTML = map((book1) => {
-
-
-//   console.log(hii)
-//   //$(".popup").toggle();
-//   document.querySelector('.flex-containernew').innerHTML =`<div class="bookDesc1">
-//   <div class="bookName1" id="bookName">${book1.bookName}</div>
-//   <div class="author1" id="author">${book1.author}</div>
+ 
+//   document.querySelector('.flex-containernew').innerHTML =`
+//   <div class="bookDesc1">
+//   <div class="bookName1" id="bookName">${bookid.bookName}</div>
+//   <div class="author1" id="author">${bookid.author}</div>
 //   <div class="rating1">
 //       <div class="star1">4.5*</div>
 //       <div class="starRating1">(20)</div>
 //   </div>
 //   <div class="price1">
-//       <div class="price11">Rs. ${book1.discountPrice}</div>
-//       <div class="originalPrice1"> ${book1.price}</div>
+//       <div class="price11">Rs. ${bookid.discountPrice}</div>
+//       <div class="originalPrice1"> ${bookid.price}</div>
 //   </div>
 // </div>
 // <div class="bookDetails">
 //   <div class="bk1">Book Detail</div>
-//   <div class= "bk2"><p><p>${book1.description}</p></div>
+//   <div class= "bk2"><p><p>${bookid.description}</p></div>
 // </div>`
-// })
+
+ 
