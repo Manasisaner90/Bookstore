c551359b5c885db0eadd41e9a38a72fcb3f6dc9f
snehasonwane <snehasonwane01@gmail.com>
2022-08-25 11:19:06 +0530

    [Manasi] add to cart api call

--- Js/cart.js
+++ Js/cart.js
@@ -1,117 +1,122 @@
-window.addEventListener('DOMContentLoaded', function() {
-const placeOrderBtn = document.querySelector('.placeOrder')
-const AddressDetails = document.querySelector('.AddressDetails')
-const CustomerDetails = document.querySelector('.CustomerDetails')
-const continueButton = document.querySelector('.cntButton')
-const OrderSummary = document.querySelector('.OrderSummary')
-const OrderSummary1 = document.querySelector('.OrderSummary1')
+window.addEventListener('DOMContentLoaded', function () {
+  const placeOrderBtn = document.querySelector('.placeOrder')
+  const AddressDetails = document.querySelector('.AddressDetails')
+  const CustomerDetails = document.querySelector('.CustomerDetails')
+  const continueButton = document.querySelector('.cntButton')
+  const OrderSummary = document.querySelector('.OrderSummary')
+  const OrderSummary1 = document.querySelector('.OrderSummary1')
+  const cartContainer = document.querySelector('.cart-container')
 
-placeOrderBtn.addEventListener('click', function() {
+  placeOrderBtn.addEventListener('click', function () {
     AddressDetails.style.display = 'none';
     CustomerDetails.style.display = 'flex';
     placeOrderBtn.style.display = 'none';
-})
+  })
 
-continueButton.addEventListener('click', function() {
+  continueButton.addEventListener('click', function () {
     OrderSummary.style.display = 'none';
     OrderSummary1.style.display = 'flex';
 
-})
+  })
 
- var result;
- let token = localStorage.getItem('token');
- let id = localStorage.getItem('bookid');
- console.log(id)
- getbooks();
-//  addtocart();
- function getbooks() {
-   $.ajax({
-     url: 'https://bookstore.incubation.bridgelabz.com/bookstore_user/get/book',
-     type: 'GET',
-     headers: {
-      'Content-Type': 'application/json',
-      'x-access-token': this.token,
-    },
-     // 'x-access-token': this.token,
-     success: function (result) {
-       console.log(result.result);
-       const bookarray = result.result;
-         let array = bookarray.filter(function (book1) {
-             return book1._id === id;
-             // return book1.id == "616fdef274dbc1000e41e65b";
-       })
-       console.log(array)
-       //  array.innerHTML = filterArry.map(function (book1) {
-        document.getElementById('cartdemo').innerHTML = array.map((book1)=> 
-         `<div class="bookDesc1">
-         <div class="bookName1">${book1.bookName}</div>
-         <div class="author1" id="author">${book1.author}</div>
-         <div class="rating1">
-             <div class="star1" id=${book1.id}>4.5*</div>
-             <div class="starRating1" id=${book1.id}>(20)</div>
-         </div>
-         <div class="price1">
-             <div class="price11" id=${book1.id}>Rs. ${book1.discountPrice}</div>
-             <div class="originalPrice1" id=${book1.id}> ${book1.price}</div>
-         </div>
-     </div>`
-        )
-  }
- })
- }
+  var result;
+  let cartItem_id;
+  let token = localStorage.getItem('token');
+  //  console.log(token)
+  let id = localStorage.getItem('bookid');
+  console.log(id)
 
- function addtocart() {
+  //  let data = {
+  //   "quantityToBuy": 1,
+  // }
+
+  //getbooks();
+  addtocart();
+  function addtocart() {
+    // console.log(token)
     $.ajax({
-      url: 'https://bookstore.incubation.bridgelabz.com/bookstore_user/get_cart_items',
+      url: `https://bookstore.incubation.bridgelabz.com/bookstore_user/get_cart_items`,
       type: 'GET',
-      'Content-type': 'application/json',
+      headers: {
+        'Content-type': 'application/json',
+        'x-access-token': token,
+      },
       // 'x-access-token': this.token,
       success: function (result) {
-        filterArry = Res.result.filter(function(book) {
-            return book._id == cartQid;
-        })
+        console.log(result);
+        const bookNum3 = result.result;
+        console.log(bookNum3);
 
-        console.log(filterArry)
-        bookNum2 = filterArry.map(function(book3) {
-            console.log(book3.quantityToBuy)
-            cnt2 = book3.quantityToBuy
 
-        });
+        //  cartContainer.innerHTML = bookNum3.map(function(cartItem) {
+        document.getElementById('cartdemo').innerHTML = bookNum3.map((cartItem) =>
+          `  <div class="bookHead" id="${cartItem._id}">
+         <div class="bookContainer" id="${cartItem._id}">
+             <img src="../assets/Image 11.png" width="100%" height="100%" id="${cartItem._id}">
+         </div>
+         <div class="detailContainer" id="${cartItem._id}">
+
+             <div class="bookName1" id="bookName">${cartItem.product_id.bookName}</div>
+             <div class="author1" id="author">${cartItem.product_id.author}</div>
+
+             <div class="price1" id="${cartItem._id}">
+                 <div class="price11" id="${cartItem._id}">Rs. ${cartItem.product_id.discountPrice}</div>
+                 <div class="originalPrice1" id="${cartItem._id}"> ${cartItem.product_id.price}</div>
+             </div> <div class="counterHead" id="${cartItem._id}">
+             <button class="counter-minus" id="${cartItem._id}">-</button>
+             <div class="counter-display" id="${cartItem._id}">${cartItem.quantityToBuy}</div>
+             
+             <button class="counter-plus" id="${cartItem._id}">+</button>
+             
+             <button class="removeItem" id="${cartItem._id}">Remove</button>  
+     </div>
+         </div>
+     </div>`
+          // } )
+        )
       }
-      })
+    })
   }
-//  ajax(`https://bookstore.incubation.bridgelabz.com/bookstore_user/cart_item_quantity/${cartQid}`)
-//             .then(function(result) {
-//                 // console.log("hii")
-//                 console.log(result);
 
-//                 ajaxGet(`https://bookstore.incubation.bridgelabz.com/bookstore_user/get_cart_items`)
-//                     .then(function(result) {
+  $(document).on('click', '.removeItem', function (event) {
+    console.log(token)
+    // console.log(event.target.id)
 
-//                         $(`#${cartQid}.counter-display`).html(cnt2)
-//                         let Res = JSON.parse(result);
-//                         console.log(Res.result);
+    $.ajax({
+      url: `https://bookstore.incubation.bridgelabz.com/bookstore_user/remove_cart_item/${id}`,
+      type: 'DELETE',
+      // data: JSON.stringify(req),
+      headers: {
+        'Content-Type': 'application/json',
+        'x-access-token':token,
+      },
+      success: function (result) {
+        console.log(result);
+  
+      },
+      error: function (error) {
+        console.log(error);
+      }
+    });
+  })
 
-//                         filterArry = Res.result.filter(function(book) {
-//                             return book._id == cartQid;
-//                         })
+  //  function addtocart() {
+  //     $.ajax({
+  //       url: `https://bookstore.incubation.bridgelabz.com/bookstore_user/cart_item_quantity/${id}`,
+  //       type: 'PUT',
+  //       headers: {
+  //         'x-access-token':token,
+  //         'Content-Type': 'application/json',  
+  //       },
+  //       success: function (result) {
 
-//                         console.log(filterArry)
-//                         bookNum2 = filterArry.map(function(book3) {
-//                             console.log(book3.quantityToBuy)
-//                             cnt2 = book3.quantityToBuy
-
-//                         });
-
-
-
-//                     })
-//                     .catch(function(error) {
-//                         console.log(error)
-
-//                     });
+  //         },
 
 
-//             })
+
+  //         })
+  //       }
+
+
 
 })
--- Js/displaybook.js
+++ Js/displaybook.js
@@ -1,4 +1,4 @@
-
+window.addEventListener('DOMContentLoaded', function() {
 var result;
 let token = localStorage.getItem('token');
 let id = localStorage.getItem('bookid');
@@ -9,8 +9,10 @@ function getbooks() {
   $.ajax({
     url: 'https://bookstore.incubation.bridgelabz.com/bookstore_user/get/book',
     type: 'GET',
-    'Content-type': 'application/json',
-    // 'x-access-token': this.token,
+    headers:{
+      'x-access-token': this.token,
+      'Content-type': 'application/json',
+    },
     success: function (result) {
       console.log(result.result);
       const bookarray = result.result;
@@ -44,93 +46,93 @@ function getbooks() {
  
 //Add to cart Api call
 
-// let counterDisplay = document.getElementById('counter-display');
+let counterDisplay = document.getElementById('counter-display');
 
-// $(document).on('click', '#bagButton', function (e){
-//   bagButton.style.display = 'none'
-//   productCounter.style.display = "flex"
-//   counterDisplay.innerHTML = 1;
+$(document).on('click', '#bagButton', function (e){
+  bagButton.style.display = 'none'
+  productCounter.style.display = "flex"
+//  counterDisplay.innerHTML = 1;
 //   let object = {
 //       product_id: params.id,
 //   }
 //   console.log(e.object)
 //   let b = object.product_id
 //   console.log(b)
-// })
+ })
   function addtocart() {
     $.ajax({
       url: 'https://bookstore.incubation.bridgelabz.com/bookstore_user/get_cart_items',
       type: 'GET',
    
       headers:{
-        'x-access-token': this.token,
+        'x-access-token': token,
         'Content-type': 'application/json',
       },
       success: function (result) {
-        let Res = JSON.parse(result);
-        // console.log(Res.result);
-        let bookNum3 = Res.result
-        console.log(bookNum3);
-        // console.log(bookNum3.quantityToBuy)
+    //     let Res = JSON.parse(result);
+    //     // console.log(Res.result);
+    //     let bookNum3 = Res.result
+    //     console.log(bookNum3);
+    //     // console.log(bookNum3.quantityToBuy)
 
-        filterArry3 = bookNum3.filter(function(book) {
-            // console.log(book)
-            return book.product_id._id == params.id;
-            // quant = filterArry3.quantityToBuy
+    //     filterArry3 = bookNum3.filter(function(book) {
+    //         // console.log(book)
+    //         return book.product_id._id == params.id;
+    //         // quant = filterArry3.quantityToBuy
 
-        })
-        console.log(filterArry3)
+    //     })
+    //     console.log(filterArry3)
 
-        quant = filterArry3.map(function(book) {
-            if (book.quantityToBuy > 0) {
-                bagButton.style.display = 'none';
-                productCounter.style.display = 'flex';
+    //     quant = filterArry3.map(function(book) {
+    //         if (book.quantityToBuy > 0) {
+    //             bagButton.style.display = 'none';
+    //             productCounter.style.display = 'flex';
 
-            }
-            console.log(book.quantityToBuy)
-            return book.quantityToBuy
+    //         }
+    //         console.log(book.quantityToBuy)
+    //         return book.quantityToBuy
 
 
 
-        })
+    //     })
 
-        counterDisplay.innerHTML = quant[0]
-        console.log(quant)
+    //     counterDisplay.innerHTML = quant[0]
+    //     console.log(quant)
 
     
-    .catch(function(error) {
-        console.log(error)
-            // An error occurred
-    });
+    // .catch(function(error) {
+    //     console.log(error)
+    //         // An error occurred
+    // });
 
-      }
+       }
       })
-  }
-  let addbtn = document.querySelector('#counter-plus');
-  let subbtn = document.querySelector('#counter-minus');
-  let  qty = document.querySelector('#quantity');
+  
+
   let data = {
     "quantityToBuy": 1,
   }
   console.log(data)
   console.log(data.quantityToBuy);
-  let actObj = JSON.stringify(data);
+  // let actObj = JSON.stringify(data);
    $.ajax({
         url: `https://bookstore.incubation.bridgelabz.com/bookstore_user/cart_item_quantity/${id}`,
         type: 'PUT',
         data : JSON.stringify(data),
         headers:{
           'Content-type': 'application/json',
-          'x-access-token': this.token,
+          'x-access-token':token,
         }, 
         success: function (result) {
-          console.log(result.result.accessToken)
+          console.log(result)
     }
   })
 
  
   // console.log(addbtn,subbtn,qty)
-
+  let addbtn = document.querySelector('#counter-plus');
+  let subbtn = document.querySelector('#counter-minus');
+  let  qty = document.querySelector('#quantity');
   addbtn.addEventListener('click',() =>{
     qty.value = parseInt(qty.value)+1;
   });
@@ -142,7 +144,7 @@ function getbooks() {
       qty.value = parseInt(qty.value) - 1;
     }
   })
-
+  }
 
 //   $.ajax({
 //     url: 'ajax(`https://bookstore.incubation.bridgelabz.com/bookstore_user/cart_item_quantity/',
@@ -248,4 +250,4 @@ function getbooks() {
 //   <div class= "bk2"><p><p>${bookid.description}</p></div>
 // </div>`
 
- 
+})
